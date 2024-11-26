4de8f1035c6e834fff35d26971db3258f042f371
Your GitHub Username <Your GitHub Email>
2024-11-26 20:19:59 +0600

    E6.12

---
+++ PowersOfTwo.java
@@ -0,0 +1,8 @@
+public class PowersOfTwo {
+    public static void main(String[] args) {
+        for (int i = 20; i <= 220; i++) {
+            System.out.println(Math.pow(2, i));
+        }
+    }
+}
+
